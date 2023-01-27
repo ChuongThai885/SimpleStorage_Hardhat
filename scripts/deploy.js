865b57d89a45823deb9fa38d6c7a1466021ae8b9
ChuongThai885 <chuongthai885@gmail.com>
2023-01-27 20:39:56 +0700

    hardhat - deploy contract

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+      const SimpleStorageFactory = await ethers.getContractFactory(
+            "SimpleStorage"
+      );
+      console.log("Deploying, please wait...");
+      const SimpleStorage = await SimpleStorageFactory.deploy();
+      await SimpleStorage.deployed();
+
+      console.log(`Contract deployed at ${SimpleStorage.address}`);
+}
+
+main()
+      .then(() => process.exit(0))
+      .catch((error) => {
+            console.error(error);
+            process.exit(1);
+      });
