90ad8c0c3adfa38f4188e396c0798a4866cf76a6
ChuongThai885 <chuongthai885@gmail.com>
2023-01-28 03:34:47 +0700

    hardhat - deploy on goerli test network , interact with contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,5 @@
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat");
+require("dotenv").config();
 
 async function main() {
       const SimpleStorageFactory = await ethers.getContractFactory(
@@ -9,6 +10,36 @@ async function main() {
       await SimpleStorage.deployed();
 
       console.log(`Contract deployed at ${SimpleStorage.address}`);
+
+      if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+            console.log("Waiting for block txes...");
+            await SimpleStorage.deployTransaction.wait(6);
+            verify(SimpleStorage.address, []);
+      }
+
+      const currentValue = await SimpleStorage.retrieve();
+      console.log(`Current value is ${currentValue}`);
+
+      const transactionResponse = await SimpleStorage.store(777);
+      await transactionResponse.wait(1);
+      const updatedValue = await SimpleStorage.retrieve();
+      console.log(`Updated value is ${updatedValue}`);
+}
+
+async function verify(contractAddress, args) {
+      console.log("Verifying contract...");
+      try {
+            await run("verify:verify", {
+                  address: contractAddress,
+                  constructorArgument: args,
+            });
+      } catch (e) {
+            if (e.message.toLowerCase().includes("already verified")) {
+                  console.log("Already verified!");
+            } else {
+                  console.log(e);
+            }
+      }
 }
 
 main()
